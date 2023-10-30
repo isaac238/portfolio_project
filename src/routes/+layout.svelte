4ee1066e328eeccf80b2630cbcee149f9c1795e5
isaac238 <isaac@isaac238.dev>
2023-10-30 13:48:38 +0000

    Switched path to css to relative

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,7 @@
 	import ThemeSwitcher from "$lib/ThemeSwitcher.svelte";
 
 </script>
-<link rel="stylesheet" href="src/global.css" />
+<link rel="stylesheet" href="../global.css" />
 <ThemeSwitcher />
 <main class="bg-gray-100 dark:bg-gray-900 dark:text-white w-full lg:w-1/2 my-0 mx-auto p-4">
 <slot />
