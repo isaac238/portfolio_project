63972a3e93e61eb8121052c763723c121b4e703b
isaac238 <isaac@isaac238.dev>
2023-10-30 14:03:19 +0000

    Added header properties

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,11 @@
 	import '../global.css';
 </script>
 
+<svelte:head>
+<title>Portfolio | Isaac</title>
+<meta name="description" content="Isaac's portfolio website" />
+</svelte:head>
+
 <ThemeSwitcher />
 <main class="bg-gray-100 dark:bg-gray-900 dark:text-white w-full lg:w-1/2 my-0 mx-auto p-4">
 <slot />
